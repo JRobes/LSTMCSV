6c9f84a47af792a24e7e2f8a94fa9c2515f91ce1
JRobes <tarres.javier@gmail.com>
2025-01-08 07:58:53 +0100

    werewr

---
+++ src/main/java/dual_lstm_csv_manipulation/App.java
@@ -0,0 +1,13 @@
+package dual_lstm_csv_manipulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

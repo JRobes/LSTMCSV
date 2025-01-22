8c0502ae1e7d97a0e6b22b77a1f76057b59001e9
JRobes <tarres.javier@gmail.com>
2025-01-22 08:19:08 +0100

    Oficina - Se ha cambiado el parseado de los valores en el csv. Se ha añadido el tratamiento de los datos del IBEX

--- src/main/java/dual_lstm_csv_manipulation/App.java
+++ src/main/java/dual_lstm_csv_manipulation/App.java
@@ -2,14 +2,12 @@ package dual_lstm_csv_manipulation;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
-import org.datavec.api.records.reader.impl.collection.CollectionSequenceRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.transform.MathOp;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.analysis.DataAnalysis;
 import org.datavec.api.transform.analysis.columns.ColumnAnalysis;
 import org.datavec.api.transform.condition.column.NullWritableColumnCondition;
-import org.datavec.api.transform.filter.ConditionFilter;
 import org.datavec.api.transform.filter.FilterInvalidValues;
 import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.transform.transform.string.ReplaceStringTransform;
@@ -35,6 +33,7 @@ import java.util.List;
 public class App 
 {
     static String fileName = "ACX-short.csv";
+    static String fileNameIBEX = "IBEX35-short.csv";
     //static String path = "C:\\Users\\jrobes\\IdeaProjects\\LSTMCSV\\ACX-short.csv";
     //static String path = "C:\\Users\\COTERENA\\IdeaProjects\\LSTMCSV\\ACX-short.csv";
     public static void main( String[] args ) throws IOException, NoSuchFieldException, IllegalAccessException, InterruptedException {
@@ -48,6 +47,7 @@ public class App
 
         System.out.println("Dos niveles arriba: " + path2LevelsUp.toAbsolutePath() + File.separator + fileName);
         String path = path2LevelsUp.toAbsolutePath() + File.separator + fileName;
+        String pathIBEX = path2LevelsUp.toAbsolutePath() + File.separator + fileNameIBEX;
 
         Schema inputDataSchema = new Schema.Builder()
                 .addColumnString("DateTimeString", ".*", 1, null)
@@ -63,25 +63,23 @@ public class App
         System.out.println(inputDataSchema);
 
         // Crear el TransformProcess
-        Map<String, String> replacements = new HashMap<>();
-        replacements.put(",", "."); // Reemplazar ',' por '.'
+        //Map<String, String> replacements = new HashMap<>();
+        //replacements.put(",", "."); // Reemplazar ',' por '.'
 
         TransformProcess tp = new TransformProcess.Builder(inputDataSchema)
 
                 .filter(new FilterInvalidValues("DateTimeString", "Last", "Open", "Max","Min", "Vol","Per"))
 
-                .transform(new ReplaceStringTransform("Last", replacements))
+                .transform(new InvestingNumberTransform("Last"))
                 .convertToDouble("Last")
-                .transform(new ReplaceStringTransform("Open", replacements))
+                .transform(new InvestingNumberTransform("Open"))
                 .convertToDouble("Open")
-                .transform(new ReplaceStringTransform("Max", replacements))
+                .transform(new InvestingNumberTransform("Max"))
                 .convertToDouble("Max")
-                .transform(new ReplaceStringTransform("Min", replacements))
+                .transform(new InvestingNumberTransform("Min"))
                 .convertToDouble("Min")
-                .transform(new ReplaceStringTransform("Vol", replacements))
-                .transform(new SuffixTransform("Vol"))
+                .transform(new InvestingNumberTransform("Vol"))
                 .convertToDouble("Vol")
-                .transform(new ReplaceStringTransform("Per", replacements))
                 .transform(new PercentTransform("Per"))
                 .convertToDouble("Per")
                 .doubleColumnsMathOp("Diff", MathOp.Subtract, "Max", "Min")
@@ -91,15 +89,30 @@ public class App
                 .filter(new NullWritableColumnCondition("Last"))
                 .build();
 
-        Schema outputSchema = tp.getFinalSchema();
+        TransformProcess tpIBEX = new TransformProcess.Builder(inputDataSchema)
+                .removeColumns("Open", "Max","Min", "Vol","Per")
+                .filter(new FilterInvalidValues("DateTimeString", "Last"))
+                .transform(new InvestingNumberTransform("Last"))
+                .convertToDouble("Last")
+                .stringToTimeTransform("DateTimeString","DD.MM.YYYY", DateTimeZone.UTC)
+                .renameColumn("DateTimeString", "Date")
+                .build();
 
-        System.out.println("\n\nSchema after transforming data:");
+
+        Schema outputSchema = tp.getFinalSchema();
+        Schema outputSchemaIBEX = tpIBEX.getFinalSchema();
+
+        System.out.println("\n\nSchemas after transforming data:");
         System.out.println(outputSchema);
+        System.out.println(outputSchemaIBEX);
 
         // Paso 3: Leer los datos con CSVRecordReader
         int skipNumLines = 1; // Saltar la primera línea (cabecera)
         RecordReader recordReader = new CSVRecordReader(skipNumLines, ',', '"');
         recordReader.initialize(new FileSplit(new File(path)));
+
+        RecordReader recordReaderIBEX = new CSVRecordReader(skipNumLines, ',', '"');
+        recordReaderIBEX.initialize(new FileSplit(new File(pathIBEX)));
 
         System.out.println("Original data");
         // Paso 4: Almacenar los registros en una lista
@@ -108,12 +121,25 @@ public class App
             originalData.add(recordReader.next());
         }
 
+        List<List<Writable>> originalDataIBEX = new ArrayList<>();
+        while (recordReaderIBEX.hasNext()) {
+            originalDataIBEX.add(recordReaderIBEX.next());
+        }
+
         // Paso 5: Aplicar el TransformProcess localmente
         List<List<Writable>> transformedData = LocalTransformExecutor.execute(originalData, tp);
+        List<List<Writable>> transformedDataIBEX = LocalTransformExecutor.execute(originalDataIBEX, tpIBEX);
+
 
         // Paso 6: Imprimir los datos transformados
-        System.out.println("Datos transformados..." + transformedData.size());
+        System.out.println("Datos transformados...\nNúmero de filas: " + transformedData.size());
         for (List<Writable> record : transformedData) {
+            System.out.println(record);
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Datos transformados...\nNúmero de filas: " + transformedDataIBEX.size());
+        for (List<Writable> record : transformedDataIBEX) {
             System.out.println(record);
         }
 
--- src/main/java/dual_lstm_csv_manipulation/InvestingNumberTransform.java
+++ src/main/java/dual_lstm_csv_manipulation/InvestingNumberTransform.java
@@ -1,19 +1,13 @@
 package dual_lstm_csv_manipulation;
 
-import org.datavec.api.transform.schema.Schema;
-import org.datavec.api.transform.transform.BaseTransform;
 import org.datavec.api.transform.transform.string.BaseStringTransform;
-import org.datavec.api.writable.DoubleWritable;
 import org.datavec.api.writable.Text;
 import org.datavec.api.writable.Writable;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class SuffixTransform extends BaseStringTransform {
+public class InvestingNumberTransform extends BaseStringTransform {
 
 
-    public SuffixTransform(String columnName) {
+    public InvestingNumberTransform(String columnName) {
         super(columnName);
     }
 
@@ -34,11 +28,11 @@ public class SuffixTransform extends BaseStringTransform {
     private String changeString(String value){
         Double result;
         if (value.endsWith("K")) {
-            result = Double.parseDouble(value.substring(0, value.length() - 1)) * 1000;
+            result = Double.parseDouble(value.substring(0, value.length() - 1).replace(".","").replace(',','.')) * 1000;
         } else if (value.endsWith("M")) {
-            result = Double.parseDouble(value.substring(0, value.length() - 1).replace(',','.')) * 1000000;
+            result = Double.parseDouble(value.substring(0, value.length() - 1).replace(".","").replace(',','.')) * 1000000;
         } else {
-            result = Double.parseDouble(value.replace(',','.')); // Sin sufijo, se convierte directamente a Double
+            result = Double.parseDouble(value.replace(".","").replace(',','.')); // Sin sufijo, se convierte directamente a Double
         }
         return String.valueOf(result);
     }

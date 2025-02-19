24171c125d4a49b0d6367727acbcc3a5399b794a
Portatil-Window <tarres.javier@gmail.com>
2025-02-19 19:15:49 +0100

    Casa - Refactorizando

---
+++ src/main/java/dual_lstm_csv_manipulation/AA_DataFromCsv2LSTM.java
@@ -0,0 +1,7 @@
+package dual_lstm_csv_manipulation;
+
+public abstract class AA_DataFromCsv2LSTM implements IDataPreparation{
+    private int percentOfTraining;
+    private int numFeatures;
+    private int numLabels;
+}

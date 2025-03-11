7169159365ef3f4723e80c3f27d40e6b695a8cec
JRobes <tarres.javier@gmail.com>
2025-03-11 14:48:18 +0100

    Oficina - Nuevo formato de INDArray

--- src/main/java/dual_lstm_csv_manipulation/App.java
+++ src/main/java/dual_lstm_csv_manipulation/App.java
@@ -24,17 +24,23 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Hello world!
+ *
  *
  */
 public class App 
 {
     private static final Logger log = LoggerFactory.getLogger(App.class);
-    //static String fileName = "ACX-short.csv";
-    static String fileName = "ACX-2015-2025.csv";
+    static String fileName = "ACX-short.csv";
+    //static String fileName = "ACX-2015-2025.csv";
     static String fileNameIBEX = "IBEX35-short.csv";
     //static String path = "C:\\Users\\jrobes\\IdeaProjects\\LSTMCSV\\ACX-short.csv";
     //static String path = "C:\\Users\\COTERENA\\IdeaProjects\\LSTMCSV\\ACX-short.csv";
+    static double percentOfTraining = 0.7;
+    //LONGITUD DE SECUENCIA
+    static int sequenceLength = 3;
+    //INDICA CUAL ES LA COLUMNA QUE TIENE EL LABEL (EMPIEZA POR 0)
+    static int columnOfLabels = 0;
+    //EL NUMERO DE FEATURES ESTÁ DEFINIDO POR EL MNUMERO DE COLUMNAS DEL ARCHIVO DE ENTRADA
     public static void main( String[] args ) throws IOException, NoSuchFieldException, IllegalAccessException, InterruptedException {
 
         System.out.println( "Hello World!" );
@@ -57,17 +63,21 @@ public class App
                     .toArray(String[]::new);
             data.add(stringArray);
         }
-        //REVERTIR LOS DATOS, YA QUE PUEDEN ESTAR
-        Collections.reverse(data);
+
+
+        // REVERTIR LOS DATOS,
+        //Collections.reverse(data);
+
+
+
         //System.out.println("DATA NUM DE FEATURES EN ARCHIVO: " + data.get(0).length);
         int numFeatures = data.get(0).length;
         int numLabels = 1;
         int foreseenDays = 1;
 
-        int sequenceLength = 3;
 
         int numSamples = data.size();
-        double percentOfTraining = 0.7;
+
         int numberOfTrainingItems = (int)Math.round(percentOfTraining * joinedData.size());
         List<String[]> trainingData = data.subList(0, numberOfTrainingItems);
         List<String[]> testData = data.subList(numberOfTrainingItems -  sequenceLength + 1, data.size());
@@ -81,32 +91,41 @@ public class App
         System.out.println("Numero de test Items: " + testData.size());
         System.out.println("-----------------------------------------------------");
 
-       // System.out.println("@@@@@@@@@@@@@@@@ Data entero @@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-       // for(String[] e : data){
-       //     System.out.println(Arrays.toString(e));
-       // }
+        System.out.println("@@@@@@@@@@@@@@@@ Data entero @@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        //for(String[] e : data){
+        //    System.out.println(Arrays.toString(e));
+        //}
 
         System.out.println("@@@@@@@@@@@@@@@@ Training data @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        //for(String[] e : trainingData){
-        //    System.out.println(Arrays.toString(e));
-        //}
+        for(String[] e : trainingData){
+            System.out.println(Arrays.toString(e));
+        }
         System.out.println("@@@@@@@@@@@@@@@@ Test data @@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        //for(String[] e : testData){
-        //    System.out.println(Arrays.toString(e));
-        //}
+        for(String[] e : testData){
+            System.out.println(Arrays.toString(e));
+        }
 
-        INDArray[] trainingFeaturesAndLabels = new INDArray[2];
-        trainingFeaturesAndLabels = getFeaturesAndLabels(trainingData, sequenceLength, numFeatures);
-
-        INDArray[] testFeaturesAndLabels = new INDArray[2];
-        testFeaturesAndLabels = getFeaturesAndLabels(testData, sequenceLength, numFeatures);
+        //getFeaturesAndLabels3(data, columnOfLabels, sequenceLength);
 
 
+
+
+
+        /*
+
+        //INDArrays
+        INDArray[] trainingFeaturesAndLabels = getFeaturesAndLabels(trainingData, sequenceLength, numFeatures);
+        INDArray[] testFeaturesAndLabels = getFeaturesAndLabels(testData, sequenceLength, numFeatures);
         System.out.println("-----------------------------------------------------");
 
 
         DataSet trainDataSet = new DataSet(trainingFeaturesAndLabels[0],trainingFeaturesAndLabels[1]);
         DataSet testDataSet = new DataSet(testFeaturesAndLabels[0], testFeaturesAndLabels[1]);
+
+        System.out.println("Train dataSet features:");
+        System.out.println(trainDataSet.getFeatures());
+        System.out.println("Train dataSet labels:");
+        System.out.println(trainDataSet.getLabels());
 
         System.out.println("Test dataSet features:");
         //System.out.println(testDataSet.getFeatures());
@@ -120,8 +139,19 @@ public class App
         //System.out.println(testDataSet.getFeatures());
 
         System.out.println("Train dataSet features normalized:");
-        System.out.println(trainDataSet.getFeatures());
+        //System.out.println(trainDataSet.getFeatures());
 
+        */
+
+
+
+
+
+
+
+
+
+        /*
         //UIServer uiServer = UIServer.getInstance();
 
         //UIServer uiServer = UIServer.getInstance();
@@ -131,7 +161,7 @@ public class App
         //LSTMModel model = new LSTMModel();
         MultiLayerNetwork network = LSTMModel.buildModel(numFeatures, 128, 64, 0, statsStorage);
         //uiServer.attach(statsStorage);
-        for (int epoch = 0; epoch < 1000; epoch++) {
+        for (int epoch = 0; epoch < 100; epoch++) {
             if(epoch%10 == 0 )
                 System.out.println("epoch: " + epoch);
             network.fit(trainDataSet);
@@ -147,6 +177,11 @@ public class App
             System.out.println(shapel);
         }
         System.out.println(testPredicted);
+        */
+
+
+
+
 
 
         /*
@@ -181,6 +216,51 @@ public class App
 
     }
 
+    // El número de features se saca del numero de elemento del String[]
+    // La longitud total se saca del size de la Lista
+    // Hay que indicar qué columna tiene los label
+    private static void getFeaturesAndLabels3(List<String[]> data, int columnOfLabels, int sequenceLength){
+        System.out.println("Numero de strings en array data (features): " + data.get(0).length);
+        System.out.println("Número de elementos (sequence length): " + data.size());
+        int dataSize = data.size();
+        int numFeatures = data.get(0).length;
+        // Paso 1: Separar features y labels
+        double[][] features = new double[dataSize][numFeatures];
+        double[] labels = new double[dataSize];
+
+        for (int i = 0; i < dataSize; i++) {
+            for (int j = 0; j < numFeatures; j++) {
+                features[i][j] = Double.parseDouble(data.get(i)[j]);
+            }
+            labels[i] = Double.parseDouble(data.get(i)[columnOfLabels]);
+        }
+
+        List<DataSet> dataSets = new ArrayList<>();
+
+        for (int i = sequenceLength; i < dataSize; i++) {
+            // Crear secuencia de features
+            INDArray input = Nd4j.create(sequenceLength, numFeatures);
+            for (int j = 0; j < sequenceLength; j++) {
+                input.putRow(j, Nd4j.create(features[i - sequenceLength + j]));
+            }
+
+            // Crear label (el siguiente valor del label)
+            INDArray output = Nd4j.create(new double[]{labels[i]});
+
+            System.out.println(input);
+            System.out.println("---------------------------------");
+            System.out.println(output);
+            System.out.println("=================================");
+
+            // Crear DataSet
+            DataSet dataSet = new DataSet(input, output);
+            dataSets.add(dataSet);
+        }
+
+
+
+    }
+
 
 
     private static INDArray[] getFeaturesAndLabels(List<String[]> data, int sequenceLength, int numFeatures) {
@@ -190,7 +270,7 @@ public class App
         System.out.println("Num features: " + numFeatures);
         System.out.println("Numero de strings en array data: " + data.get(0).length);
         int numSequences = data.size() - sequenceLength; // 100 - 5 + 1 = 96
-        double[][][] features = new double[numSequences][numFeatures][sequenceLength];
+        double[][][] features   = new double[numSequences][numFeatures][sequenceLength];
         double[][][] labels     = new double[numSequences][numLabels][sequenceLength];
 
         // Procesar los datos
@@ -201,11 +281,56 @@ public class App
                 }
             }
             // Leer el target (primera columna, que es el target)
-           // labels[i][0] = Double.parseDouble(data.get(i + sequenceLength)[0]);
+            // labels[i][0] = Double.parseDouble(data.get(i + sequenceLength)[0]);
         }
         // Procesar los datos
         for (int i = 0; i < numSequences; i++) {
             for (int j = 0; j < sequenceLength; j++) {
+                //int numLabels = 1;
+                for(int k = 0; k < numLabels; k++){
+                    //features[i][k][j] = Double.parseDouble(data.get(i + j)[k]);
+                    labels[i][k][j] = Double.parseDouble(data.get(i + j)[k]);
+                }
+            }
+            // Leer el target (primera columna, que es el target)
+            //labels[i][0] = Double.parseDouble(data.get(i + sequenceLength)[0]);
+        }
+
+
+
+        INDArray featureArray = Nd4j.create(features);
+        INDArray labelArray = Nd4j.create(labels);
+        //System.out.println("@@@@@@ INDArray features @@@@@@@@@: \n" + featureArray); // [numSamples - sequenceLength, sequenceLength, numFeatures]
+        //System.out.println("Forma de labels: " + Arrays.toString(labelArray.shape()));
+        //System.out.println("@@@@@@ INDArray labels @@@@@@@@@@@: \n" + labelArray);
+        return new INDArray[]{featureArray, labelArray};
+    }
+
+
+    private static INDArray[] getFeaturesAndLabels2(List<String[]> data, int sequenceLength, int numFeatures) {
+        // TRAIN DATA
+        // Inicializar arrays para features y labels
+        int numLabels = 1;
+        int sequenceLengthLabels = 1;
+        System.out.println("Num features: " + numFeatures);
+        System.out.println("Numero de strings en array data: " + data.get(0).length);
+        int numSequences = data.size() - sequenceLength; // 100 - 5 + 1 = 96
+        double[][][] features   = new double[numSequences][numFeatures][sequenceLength];
+        double[][][] labels     = new double[numSequences][numLabels][sequenceLengthLabels];
+
+        // Procesar los datos
+        for (int i = 0; i < numSequences; i++) {
+            for (int j = 0; j < sequenceLength; j++) {
+                for(int k = 0; k < numFeatures; k++){
+                    features[i][k][j] = Double.parseDouble(data.get(i + j)[k]);
+                }
+            }
+            // Leer el target (primera columna, que es el target)
+            // labels[i][0] = Double.parseDouble(data.get(i + sequenceLength)[0]);
+        }
+        // Procesar los datos
+        for (int i = 0; i < numSequences; i++) {
+            for (int j = 0; j < sequenceLengthLabels; j++) {
                 //int numLabels = 1;
                 for(int k = 0; k < numLabels; k++){
                     //features[i][k][j] = Double.parseDouble(data.get(i + j)[k]);
